82182fa0fb6d7a7e05709490c471f6c050403758
Glynn Bird <glynn.bird@gmail.com>
2016-09-30 11:28:16 +0100

    respond first

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/getstarted', function(req, res) {
+  res.send({ok:true});
   const exec = require('child_process').exec;
-  exec('/home/wow/wow/flush.sh', (error, stdout, stderr) => {
+  exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
     }
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
-    res.send({ok:true});
   });
+
+  
 })
 
 app.listen(8000, function () {
