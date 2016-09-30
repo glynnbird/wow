d3fb318444312927c4b6eb1fc350db38a0fe3d78
Glynn Bird <glynn.bird@gmail.com>
2016-09-30 11:37:40 +0100

    links

--- public/js/init.js
+++ public/js/init.js
@@ -12,5 +12,10 @@ var getstarted = function() {
 
     $('.button-collapse').sideNav();
 
+    var dm = window.location.hostname;
+    $('#sss-button').attr('href', 'http://' + dm + ':' + 6001);
+    $('#sas-button').attr('href', 'http://' + dm + ':' + 6002);
+    $('#scs-button').attr('href', 'http://' + dm + ':' + 6003);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
